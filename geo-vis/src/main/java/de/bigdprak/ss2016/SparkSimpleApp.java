76dd7d1c04442ac12b3b04f8bbb51b5bf2ff62c8
Balthorius <d-alexander@gmx.net>
2016-05-26 13:37:32 +0200

    added some example classes for getting started with spark and hadoop in java

--- geo-vis/src/main/java/de/bigdprak/ss2016/SparkSimpleApp.java
+++ geo-vis/src/main/java/de/bigdprak/ss2016/SparkSimpleApp.java
@@ -1,25 +1,67 @@
 package de.bigdprak.ss2016;
 
 /* SimpleApp.java */
-import org.apache.spark.api.java.*;
+import java.util.Arrays;
+
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
 
 public class SparkSimpleApp {
-  public static void main(String[] args) {
-    String logFile = "~/Schreibtisch/spark-1.6.1/README.md"; // Should be some file on your system
-    SparkConf conf = new SparkConf().setAppName("Simple Application");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-    JavaRDD<String> logData = sc.textFile(logFile).cache();
+	public static void main(String[] args) {
+		String logFile = "~/Schreibtisch/spark-1.6.1/README.md"; // Should be
+																	// some file
+																	// on your
+																	// system
+		SparkConf conf = new SparkConf().setAppName("Simple Application");
+		conf.setMaster("hdfs://wdi06.informatik.uni-leipzig.de:8020");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> logData = sc.textFile(logFile).cache();
 
-    long numAs = logData.filter(new Function<String, Boolean>() {
-      public Boolean call(String s) { return s.contains("a"); }
-    }).count();
+		long numAs = logData.filter(new Function<String, Boolean>() {
+			public Boolean call(String s) {
+				return s.contains("a");
+			}
+		}).count();
 
-    long numBs = logData.filter(new Function<String, Boolean>() {
-      public Boolean call(String s) { return s.contains("b"); }
-    }).count();
+		long numBs = logData.filter(new Function<String, Boolean>() {
+			public Boolean call(String s) {
+				return s.contains("b");
+			}
+		}).count();
 
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-  }
+		System.out.println("Lines with a: " + numAs + ", lines with b: "
+				+ numBs);
+		
+		// ---------------------------------------------------------------------
+
+		JavaRDD<String> textFile = sc.textFile("hdfs://...");
+		JavaRDD<String> words = textFile
+				.flatMap(new FlatMapFunction<String, String>() {
+					public Iterable<String> call(String s) {
+						return Arrays.asList(s.split(" "));
+					}
+				});
+		JavaPairRDD<String, Integer> pairs = words
+				.mapToPair(new PairFunction<String, String, Integer>() {
+					public Tuple2<String, Integer> call(String s) {
+						return new Tuple2<String, Integer>(s, 1);
+					}
+				});
+		JavaPairRDD<String, Integer> counts = pairs
+				.reduceByKey(new Function2<Integer, Integer, Integer>() {
+					public Integer call(Integer a, Integer b) {
+						return a + b;
+					}
+				});
+		counts.saveAsTextFile("hdfs://...");
+
+	}
 }
