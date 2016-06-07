0adfe080ad5c09b02797ff0ea627da438ff3af92
Balthorius <d-alexander@gmx.net>
2016-06-07 11:07:26 +0200

    doc aktualisiert, logging reduziert

--- geo-vis/src/main/java/de/bigdprak/ss2016/SimpleApp.java
+++ geo-vis/src/main/java/de/bigdprak/ss2016/SimpleApp.java
@@ -1,6 +1,8 @@
 package de.bigdprak.ss2016;
 
 /* SimpleApp.java */
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
@@ -13,7 +15,8 @@ public class SimpleApp {
 		String master;
 		String input_file;
 
-		input_file = "/home/balthorius/progs/hadoop/input/Affiliations.txt";
+		//input_file = "/home/balthorius/progs/hadoop/input/Affiliations.txt";
+		input_file = "/home/balthorius/progs/hadoop/input/PaperAuthorAffiliations.txt";
 		master = "local";
 
 		//input_file = "/home/bigprak/progs/hadoop/input/Affiliations.txt";
@@ -22,6 +25,10 @@ public class SimpleApp {
 		//master = "spark://wdi06.informatik.uni-leipzig.de:7077";
 		
 		SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster(master);
+		
+	    Logger.getLogger("org").setLevel(Level.ERROR);
+	    Logger.getLogger("akka").setLevel(Level.ERROR);
+	
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		// JavaRDD<String> logData = sc.textFile(logFile).cache();
 		JavaRDD<String> logData = sc.textFile(input_file);//.cache();
@@ -53,25 +60,29 @@ public class SimpleApp {
 		}
 		
 		// mapping the length of each line to that line
+		System.out.println("Do the mapping...");
 		JavaRDD<Integer> lineLengths = lines.map(new GetLength());
 		// reducing all line lengths to the aggregated sum
+		System.out.println("Do the reduce job...");
 		int totalLength = lineLengths.reduce(new Sum());
+		System.out.println("total length: " + totalLength);
 		
-		
+		/*
+		System.out.println("Do the filter a job...");
 		long numAs = logData.filter(new Function<String, Boolean>() {
 			public Boolean call(String s) {
 				return s.contains("a");
 			}
 		}).count();
 
+		System.out.println("Do the filter b job...");
 		long numBs = logData.filter(new Function<String, Boolean>() {
 			public Boolean call(String s) {
 				return s.contains("b");
 			}
 		}).count();
-
 		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-		System.out.println("total length: " + totalLength);
+		*/
 		sc.close();
 	}
 }
